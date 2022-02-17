bf6c5f0209243c41e7cb8323a377cb50fe66337d
SHIVAM-NANDANE <shivamn@coreflexsolutions.com>
2022-02-17 17:22:25 +0530

    Committing kotlin code

---
+++ NewLearning.kt
@@ -0,0 +1,13 @@
+class NewLearning {
+
+}
+fun main() {
+
+
+    val myNum: Long = 15000000000L
+    println(myNum)
+    val myNum2: Float = 5.75F
+    println(myNum2)
+    val myNum3: Double = 19.99
+    println(myNum3)
+}
